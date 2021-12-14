633fe2736a1d7d8ee40350ab926fe31e38336832
Sviatoslav <sv_dubov@ukr.net>
2021-12-14 14:27:16 +0200

    Install and set Noty

--- resources/js/app.js
+++ resources/js/app.js
@@ -18,6 +18,10 @@ import {routes} from './routes';
 import User from './Helpers/User';
 window.User = User;
 
+//Import Notification Class
+import Notification from './Helpers/Notification';
+window.Notification = Notification;
+
 //Import sweetalert
 import Swal from 'sweetalert2';
 window.Swal = Swal;
