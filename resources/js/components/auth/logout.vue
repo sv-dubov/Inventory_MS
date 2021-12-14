8bb95165358b3da1867100b80e7d33fadd7881ca
Sviatoslav <sv_dubov@ukr.net>
2021-12-14 12:15:01 +0200

    Make logout

---
+++ resources/js/components/auth/logout.vue
@@ -0,0 +1,21 @@
+<template>
+
+</template>
+
+<script type="text/javascript">
+    export default {
+        created() {
+            localStorage.removeItem('token');
+            localStorage.removeItem('user');
+            Toast.fire({
+                icon: 'success',
+                title: 'Logged out successfully!'
+            });
+            this.$router.push({name: '/'})
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
